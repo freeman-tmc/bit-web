96b433fa9f5cb22aaafd1b466ef7accfb7eed622
freeman-tmc <freeman.tomic84@gmail.com>
2018-03-01 22:50:40 +0100

    mario done

--- week5/mario/main.js
+++ week5/mario/main.js
@@ -1,32 +1,93 @@
 
+var running = document.querySelector('#running');
+var standing = document.querySelector('#mario');
+var jumping = document.querySelector('#jumping');
+
+var slide = 0;
+var interval;
+
+function ground() {
+    document.body.style.backgroundPosition = slide + 'px bottom';
+    slide += -15;
+}
+
 
 function run() {
-    var mario = document.querySelector('#mario');
-    mario.setAttribute('src', 'img/mario_running.gif');
+    standing.style.display = 'none';
+    running.style.display = 'block';
+    if (interval == undefined) {
+        interval = setInterval(ground, 100);
+    }
 }
 
 function stop() {
-    var mario = document.querySelector('#mario');
-    mario.setAttribute('src', 'img/mario.png');
+    running.style.display = 'none';
+    standing.style.display = 'block';
+    interval = clearInterval(interval);
+}
+
+
+function goUp() {
+    
+    jumping.style.bottom = altitude + 'px';
+    altitude += 10;
+    if (altitude == 233) {
+        clearInterval(up);
+        down = setInterval(goDown, 50);
+    }
+}
+
+function goDown() {
+    altitude += -10;
+    jumping.style.bottom = altitude + 'px';
+    if (altitude == 83) {
+        clearInterval(down);
+        document.body.addEventListener('keydown', checkCode);
+        jumping.style.display = 'none';
+        standing.style.display = 'block';
+        running.style.display = 'none';
+    }
+    
+}
+
+var altitude = 83;
+var up;
+var down;
+
+function jump() {
+    if (altitude == 83) {
+        document.body.removeEventListener('keydown', checkCode);
+        jumping.style.display = 'block';
+        running.style.display = 'none';
+        standing.style.display = 'none';
+        
+        up = setInterval(goUp, 50);
+    }
+}
+
+function checkCode() {
+    if (event.keyCode == 32) {
+        jump();
+    }
 }
 
 
 
-document.body.addEventListener('keydown', function(event) {
-    if(event.keyCode == 39) {
+
+document.body.addEventListener('keydown', function (event) {
+    if (event.keyCode == 39) {
         run();
     }
-})
+});
 
-document.body.addEventListener('keyup', function(event) {
-    if(event.keyCode == 39) {
+document.body.addEventListener('keyup', function (event) {
+    if (event.keyCode == 39) {
         stop();
     }
-})
+});
+
+document.body.addEventListener('keydown', checkCode);
 
 
-// var a = 0;
-// function ground() {
-//     document.body.style.backgroundPosition
-// }
-// var b = setInterval(ground, 50);
+
+
